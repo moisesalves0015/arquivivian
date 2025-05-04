95fc76e5fb11c85e6c445077db893780d2c2276a
Moises Santos <moisesalves0015@gmail.com>
2025-05-04 15:02:46 -0300

    feat: versão mínima viável pronta para uso

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -25,7 +25,7 @@ useEffect(()=>{
 })
 
   return (
-    <div ref={displayRef} className='w[100%] m-2 px-6 pt-4 rounded br-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0' >
+    <div ref={displayRef} className='w[100%] m-2 mb-10 px-6 pt-4 rounded br-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0' >
       <Routes>
         <Route path='/' element={<DisplayHome />} />
         <Route path='/album/:id' element={<DisplayAlbum />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,19 +10,22 @@ const Navbar = () => {
 
     return (
         <>
+            <p className=' text-1xl mb-2'>Disciplina</p>
+            <p className='font-bold text-4xl mb-4'>História Pública</p>
             <div className='w-full flex justify-between items-center font-semibold' >
+
                 <div className='flex items-center gap-2' >
-                    <img onClick={()=>navigate(-1)} className='w-8 bg-black p-2 rounded-2xl cursor-pointer' src={assets.arrow_left} alt="" />
-                    <img onClick={()=>navigate(1)} className='w-8 bg-black p-2 rounded-2xl cursor-pointer' src={assets.arrow_right} alt="" />
+                    <img onClick={() => navigate(-1)} className='w-8 bg-black p-2 rounded-2xl cursor-pointer' src={assets.arrow_left} alt="" />
+                    <img onClick={() => navigate(1)} className='w-8 bg-black p-2 rounded-2xl cursor-pointer' src={assets.arrow_right} alt="" />
                 </div>
                 <div className=' items-center gap-4 hidden' > //oculto
-                   <p className='bg-white text-black text-[15px] px-4 py-1 rounded-2xl md:block cursor-pointer'>Explore Premium</p>
+                    <p className='bg-white text-black text-[15px] px-4 py-1 rounded-2xl md:block cursor-pointer'>Explore Premium</p>
                 </div>
             </div>
             <div className='hidden items-center gap-2 mt-4'>
-            <p className='bg-white text-black px-4 py-1 rounded-2xl cursor-pointer'>ALL</p>
-            <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer'>Music</p>
-            <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer'>Podcasts</p>
+                <p className='bg-white text-black px-4 py-1 rounded-2xl cursor-pointer'>ALL</p>
+                <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer'>Music</p>
+                <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer'>Podcasts</p>
             </div>
         </>
     )
